affa00dbe4b638b8baa4bc4bdec849014f956113
Manasi0101 <manasikoli1407@gmail.com>
2026-04-01 22:40:41 +0530

    Update experiment no 1.py

--- experiment no 1.py
+++ experiment no 1.py
@@ -1,5 +1,5 @@
 #integer
-Roll_no=207
+Roll_no=229
 print(type(Roll_no))
 
 #float
@@ -7,7 +7,7 @@ b=10.5
 print(type(b))
 
 #string
-c="nagesh"
+c="manasi"
 print(type(c))
 
 #boolean
@@ -23,7 +23,7 @@ f=(10,20,30)
 print(type(f))
 
 #dictionary
-g={"name":"nagesh",
+g={"name":"manasi",
    "age":20}
 print(type(g))
 
